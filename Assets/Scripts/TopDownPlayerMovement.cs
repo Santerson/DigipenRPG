7f5a09064ffe95ab57a653a652be2439696ad214
Riley <113722425+rilelaxy@users.noreply.github.com>
2024-07-19 16:30:48 -0700

    done

--- Assets/Scripts/TopDownPlayerMovement.cs
+++ Assets/Scripts/TopDownPlayerMovement.cs
@@ -6,73 +6,97 @@ public class TopDownPlayerMovement : MonoBehaviour
 {
     [Tooltip("The speed at which the player ACCELERATES at")]
     [SerializeField] float PlayerAccelerationSpeed = 5.0f;
+
     [Tooltip("The Player's max move speed")]
     [SerializeField] float PlayerMaxMoveSpeed = 3.0f;
+
     [Tooltip("The rate that the player slows down")]
     [SerializeField] float DampingCoefficient = 0.97f;
+
     Rigidbody2D RefRigidbody = null;
+    Animator animator;
+
     Vector2 position = Vector2.zero;
 
     private void Awake()
     {
         RefRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
         if (RefRigidbody == null)
         {
-            Debug.LogError("Player must have a rigidbody component");
+            Debug.LogError("Player must have a Rigidbody2D component");
+        }
+
+        if (animator == null)
+        {
+            Debug.LogError("Player must have an Animator component");
         }
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         FindObjectOfType<CanvasChecker>().hide();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         MovePlayer();
-
+        UpdateAnimator();
+        float speed = RefRigidbody.velocity.magnitude;
+        animator.SetFloat("Speed", speed);
     }
 
-    /// <summary>
-    /// Detects player input and will move the character
-    /// </summary>
     void MovePlayer()
     {
         if (RefRigidbody == null) return;
-        //Creating a move vector
+
+        // Creating a move vector
         Vector2 inputVector = Vector2.zero;
 
-        //Checking for player input on the keyboard
-        if (Input.GetKey(KeyCode.W)) { inputVector.y += 1; } //Up
-        if (Input.GetKey(KeyCode.A)) { inputVector.x -= 1; } //Left
-        if (Input.GetKey(KeyCode.S)) { inputVector.y -= 1; } //Down
-        if (Input.GetKey(KeyCode.D)) { inputVector.x += 1; } //Right
-        //Normalizing the vector so we can have the player actually move at the correct speed
+        // Checking for player input on the keyboard
+        if (Input.GetKey(KeyCode.W)) { inputVector.y += 1; } // Up
+        if (Input.GetKey(KeyCode.A)) { inputVector.x -= 1; } // Left
+        if (Input.GetKey(KeyCode.S)) { inputVector.y -= 1; } // Down
+        if (Input.GetKey(KeyCode.D)) { inputVector.x += 1; } // Right
+
+        // Normalizing the vector so we can have the player move at the correct speed
         inputVector.Normalize();
-        //Move the player
+
+        // Applying acceleration to move the player
         RefRigidbody.AddForce(inputVector * PlayerAccelerationSpeed);
 
-        //Checking if the player is moving faster than their maximum movement speed (this usually happens if you move diagonally)
+        // Checking if the player is moving faster than their maximum movement speed
         if (RefRigidbody.velocity.magnitude > PlayerMaxMoveSpeed)
         {
-            //Slow down the player so they don't go faster than they can
+            // Limiting the speed to PlayerMaxMoveSpeed
             RefRigidbody.velocity = RefRigidbody.velocity.normalized * PlayerMaxMoveSpeed;
         }
 
-        // We know the player let go of the controls if the input vector is nearly zero.
+        // Damping the movement when no input is given
         if (inputVector.sqrMagnitude <= 0.1f)
         {
-            // Quickly damp the movement when we let go of the inputs by multiplying the vector
-            // by a value less than one each frame.
+            // Applying damping effect to slow down the player
             RefRigidbody.velocity *= DampingCoefficient;
         }
 
+        // Storing current position
         position = transform.position;
     }
 
-    public void resetPosition()
+    void UpdateAnimator()
+    {
+        // Determine movement direction
+        Vector2 moveDirection = RefRigidbody.velocity.normalized;
+
+        // Update animator parameters based on movement direction
+
+        animator.SetFloat("Speed", RefRigidbody.velocity.magnitude);
+    }
+
+    // Method to reset the player's position
+    public void ResetPosition()
     {
         transform.position = position;
     }
